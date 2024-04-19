f4dc39ce97aa7dab2aa15b6460919944da2dcdde
erreps11 <167550817+erreps11@users.noreply.github.com>
2024-04-19 11:57:01 -0500

    laboratorio 2

---
+++ laboratorio1/Laboratorio2/Program.cs
@@ -0,0 +1,15 @@
+﻿// programa que suma los primeros n numeros:
+
+var n = 50;
+
+var suma = 0;
+
+for (var i = 1; i < n; i++) 
+
+{
+    suma = suma + i;
+}
+
+
+
+Console.WriteLine("la suma es: " + suma.ToString());
